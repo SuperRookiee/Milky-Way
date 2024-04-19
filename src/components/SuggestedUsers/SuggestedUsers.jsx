37492d8a6ba511a835101483f738e128ce0e194e
SuperRookie <hyunuk0417@gmail.com>
2024-04-20 01:11:23 +0900

    fix: gitignore

--- src/components/SuggestedUsers/SuggestedUsers.jsx
+++ src/components/SuggestedUsers/SuggestedUsers.jsx
@@ -29,9 +29,9 @@ const SuggestedUsers = () => {
 			))}
 
 			<Box fontSize={12} color={"gray.500"} mt={5} alignSelf={"start"}>
-				© 2023 Built By{" "}
-				<Link href='https://www.youtube.com/@asaprogrammer_' target='_blank' color='blue.500' fontSize={14}>
-					As a Programmer
+				© 2024 Built By{" "}
+				<Link href='https://github.com/SuperRookiee/Milky-Way' target='_blank' color='blue.500' fontSize={14}>
+					As a SuperRookiee
 				</Link>
 			</Box>
 		</VStack>
