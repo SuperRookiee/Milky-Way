7ce2e9d2cfeccd41fb4193fc09417d369e086490
SuperRookie <hyunuk0417@gmail.com>
2024-04-21 01:02:01 +0900

    feat: dark mode - Modal

--- src/components/Sidebar/CreatePost.jsx
+++ src/components/Sidebar/CreatePost.jsx
@@ -77,37 +77,34 @@ const CreatePost = () => {
 			<Modal isOpen={isOpen} onClose={onClose} size='xl' isCentered>
 				<ModalOverlay />
 
-				<ModalContent bg='black' border='1px solid gray'>
+				<ModalContent border='1px solid gray'>
 					<ModalHeader>Create Post</ModalHeader>
 					<ModalCloseButton />
 					<ModalBody pb={6}>
-						<Textarea
-							placeholder='Post caption...'
-							height={200}
-							value={caption}
-							onChange={(e) => setCaption(e.target.value)}
-						/>
-
 						<Input type='file' hidden ref={imageRef} onChange={handleImageChange} />
-
-						<BsFillImageFill
-							onClick={() => imageRef.current.click()}
-							style={{ marginTop: "15px", marginLeft: "5px", cursor: "pointer" }}
-							size={16}
-						/>
-						{selectedFile && (
-							<Flex mt={5} w='full' position='relative' justifyContent='center'>
+						{selectedFile ?
+							<Flex my={5} w='full' position='relative' justifyContent='center'>
 								<Image src={selectedFile} alt='Selected img' />
 								<CloseButton
 									position='absolute'
 									top={2}
 									right={2}
-									onClick={() => {
-										setSelectedFile(null);
-									}}
+									onClick={() => setSelectedFile(null)}
 								/>
 							</Flex>
-						)}
+							:
+							<BsFillImageFill
+								onClick={() => imageRef.current.click()}
+								style={{ marginBottom: "15px", marginLeft: "5px", cursor: "pointer" }}
+								size={20}
+							/>
+						}
+						<Textarea
+							placeholder='Post caption...'
+							height={150}
+							value={caption}
+							onChange={(e) => setCaption(e.target.value)}
+						/>
 					</ModalBody>
 
 					<ModalFooter>
