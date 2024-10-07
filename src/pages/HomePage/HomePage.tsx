e7974bc058228a1363fe1b5f0ff3542428808c97
SuperRookiee <superrookiee@chunjae.co.kr>
2024-10-07 16:15:32 +0900

    feat: update version

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -3,18 +3,18 @@ import FeedPosts from "../../components/FeedPosts/FeedPosts.jsx";
 import SuggestedUsers from "../../components/SuggestedUsers/SuggestedUsers.jsx";
 
 const HomePage = () => {
-	return (
-		<Container maxW='container.lg'>
-			<Flex gap={20}>
-				<Box flex={2} py={5}>
-					<FeedPosts />
-				</Box>
-				<Box flex={3} py={5} mr={20} display={{ base: "none", lg: "block" }} maxW='300px'>
-					<SuggestedUsers />
-				</Box>
-			</Flex>
-		</Container>
-	);
+    return (
+        <Container maxW='container.lg'>
+            <Flex gap={20}>
+                <Box flex={2} py={5}>
+                    <FeedPosts/>
+                </Box>
+                <Box flex={3} py={5} mr={20} display={{base: "none", lg: "block"}} maxW='300px'>
+                    <SuggestedUsers/>
+                </Box>
+            </Flex>
+        </Container>
+    );
 };
 
 export default HomePage;
